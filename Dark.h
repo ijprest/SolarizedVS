036766de28546f9cb1751e46a84ba52556a256bd
Ian Prest <github.com@ian.prest.ca>
2013-02-21 00:02:07 -0500

    Colors for some work-item fields.

--- Dark.h
+++ Dark.h
@@ -27,49 +27,69 @@
 #include "Colors.h"
 
 // This is the "Dark" theme
-#define THEME_NAME								"Solarized Dark"
-#define THEME_GUID								"{82903FA5-A781-4088-BE17-526DFECCD587}"
+#define THEME_NAME									"Solarized Dark"
+#define THEME_GUID									"{82903FA5-A781-4088-BE17-526DFECCD587}"
 
 // Symbolic names; these are the names we'll use elsewhere,
 // and we'll re-define them for different themes (e.g., light
 // vs. dark).
-#define background_accent					color_base03
-#define background								color_base02
-#define content_dim								color_base01
-#define content_faint							color_base00
-#define content_grey							color_base0
-#define content										color_base1
-#define content_bold							color_base2
-#define content_extrabold					color_base3
+#define background_accent						color_base03
+#define background									color_base02
+#define content_dim									color_base01
+#define content_faint								color_base00
+#define content_grey								color_base0
+#define content											color_base1
+#define content_bold								color_base2
+#define content_extrabold						color_base3
 
-#define theme_main								color_blue
-#define theme_accent							color_violet
-#define theme_accent2							color_cyan
-#define theme_lightbold						color_yellow
-#define theme_bold								color_orange
-#define theme_changed							color_red
+#define theme_main									color_blue
+#define theme_accent								color_violet
+#define theme_accent2								color_cyan
+#define theme_lightbold							color_yellow
+#define theme_bold									color_orange
+#define theme_changed								color_red
 
-#define theme_commentbackground		FF073E4B				// slightly brighter than color_base02; used as a comment background only
-#define theme_visiblewhitespace		FF094A5B				// dimmer than color_base01; used for visible-whitespace only
+#define theme_commentbackground			FF073E4B				// slightly brighter than color_base02; used as a comment background only
+#define theme_visiblewhitespace			FF094A5B				// dimmer than color_base01; used for visible-whitespace only
 
-#define transp_lighten						60FFFFFF
-#define transp_darken							60000000
+#define transp_lighten							60FFFFFF
+#define transp_darken								60000000
 
-#define theme_plaintext_bkg				background
-#define theme_plaintext						content
-#define theme_border							content_dim
+#define theme_plaintext_bkg					background
+#define theme_plaintext							content
+#define theme_prompttext						content_faint
+#define theme_headertext						theme_main
+#define theme_border								content_dim
+#define theme_border_active					content_faint
 
 // Tab colors
-#define theme_tab_unselected			background_accent
-#define theme_tab_unselected_text	content
+#define theme_tab_unselected				background_accent
+#define theme_tab_unselected_text		content
+#define theme_tab_active						theme_main
+#define theme_tab_active_text				content_bold
+#define theme_tab_inactive					content_grey
+#define theme_tab_inactive_text			content_bold
+#define theme_tab_hover							content_bold
+#define theme_tab_hover_text				theme_main
+#define theme_tab_provisional				color_violet
 
-#define theme_tab_active					theme_main
-#define theme_tab_active_text			content_bold
+// List Header controls
+#define theme_header								background
+#define theme_header_text						content
+#define theme_header_hover					content_dim
+#define theme_header_hover_text			content_bold
+#define theme_header_down						content_faint
+#define theme_header_down_text			content_bold
+#define theme_header_glyph					content_faint
 
-#define theme_tab_inactive				content_grey
-#define theme_tab_inactive_text		content_bold
+// Button colors
+#define theme_button								background
+#define theme_button_text						content
+#define theme_button_hover					content_bold
+#define theme_button_hover_text			theme_main
+#define theme_button_down						content_extrabold
+#define theme_button_down_text			theme_main
+#define theme_button_checked				
+#define theme_button_checked_text		
+#define theme_button_checked_border	theme_main
 
-#define theme_tab_hover						content_bold
-#define theme_tab_hover_text			theme_main
-
-#define theme_tab_provisional			color_violet
