66c824ad1cbe96e432e026799f4c7cfbd093486c
Ian Prest <github.com@ian.prest.ca>
2013-02-27 13:16:40 -0500

    Colorized work item query list.

--- Theme.h
+++ Theme.h
@@ -24,43 +24,49 @@
 // OTHER DEALINGS IN THE SOFTWARE.
 //
 
-#define transp_lighten							60FFFFFF
-#define transp_darken								60000000
+#define transp_lighten								60FFFFFF
+#define transp_darken									60000000
 
-#define theme_plaintext_bkg					background
-#define theme_plaintext							content
-#define theme_prompttext						content_faint
-#define theme_headertext						theme_main
-#define theme_border								content_dim
-#define theme_border_active					content_faint
+#define theme_plaintext_bkg						background
+#define theme_plaintext								content
+#define theme_prompttext							content_faint
+#define theme_headertext							theme_main
+#define theme_border									content_dim
+#define theme_border_active						content_faint
+
+#define theme_selected								theme_main
+#define theme_selected_text						content_bold
+#define theme_selected_inactive				content_dim
+#define theme_selected_inactive_text	content_bold
+
 
 // Tab colors
-#define theme_tab_unselected				background_accent
-#define theme_tab_unselected_text		content
-#define theme_tab_active						theme_main
-#define theme_tab_active_text				content_bold
-#define theme_tab_inactive					content_grey
-#define theme_tab_inactive_text			content_bold
-#define theme_tab_hover							content_bold
-#define theme_tab_hover_text				theme_main
-#define theme_tab_provisional				color_violet
+#define theme_tab_unselected					background_accent
+#define theme_tab_unselected_text			content
+#define theme_tab_active							theme_main
+#define theme_tab_active_text					content_bold
+#define theme_tab_inactive						content_grey
+#define theme_tab_inactive_text				content_bold
+#define theme_tab_hover								content_bold
+#define theme_tab_hover_text					theme_main
+#define theme_tab_provisional					color_violet
 
 // List Header controls
-#define theme_header								background
-#define theme_header_text						content
-#define theme_header_hover					content_dim
-#define theme_header_hover_text			content_bold
-#define theme_header_down						content_faint
-#define theme_header_down_text			content_bold
-#define theme_header_glyph					content_faint
+#define theme_header									background
+#define theme_header_text							content
+#define theme_header_hover						content_dim
+#define theme_header_hover_text				content_bold
+#define theme_header_down							content_faint
+#define theme_header_down_text				content_bold
+#define theme_header_glyph						content_faint
 
 // Button colors
-#define theme_button								background
-#define theme_button_text						content
-#define theme_button_hover					content_bold
-#define theme_button_hover_text			theme_main
-#define theme_button_down						content_extrabold
-#define theme_button_down_text			theme_main
-#define theme_button_checked				
-#define theme_button_checked_text		
-#define theme_button_checked_border	theme_main
+#define theme_button									background
+#define theme_button_text							content
+#define theme_button_hover						content_bold
+#define theme_button_hover_text				theme_main
+#define theme_button_down							content_extrabold
+#define theme_button_down_text				theme_main
+#define theme_button_checked					
+#define theme_button_checked_text			
+#define theme_button_checked_border		theme_main
